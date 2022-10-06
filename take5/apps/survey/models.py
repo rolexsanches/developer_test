b1a8d3da2f25ab8080c720cc4fe2ebfce5241989
Jefferson472 <jefferson472@hotmail.com>
2022-10-06 11:43:07 -0300

    feat: add models da pesquisa

--- take5/apps/survey/models.py
+++ take5/apps/survey/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    """Model: Pesquisa"""
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class SurveyQuestion(models.Model):
+    """Model: questões"""
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question
+
+
+class SurveyQuestionAlternative(models.Model):
+    """Model: alternativas"""
+    question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    alternative = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.alternative
+
+
+class SurveyUserAnswer(models.Model):
+    """Model: respostas do usuário"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    user_answer = models.ForeignKey(SurveyQuestionAlternative, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_answer
