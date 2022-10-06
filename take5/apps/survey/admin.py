4678dd6dfb2495d729f249da23a99beddc8e99d2
Jefferson472 <jefferson472@hotmail.com>
2022-10-06 11:43:44 -0300

    feat: add admin config

--- take5/apps/survey/admin.py
+++ take5/apps/survey/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey, SurveyQuestion, SurveyQuestionAlternative
+
+
+class QuestionInline(admin.TabularInline):
+    model = SurveyQuestion
+    extra = 0
+
+
+class AlternativeInline(admin.TabularInline):
+    model = SurveyQuestionAlternative
+    extra = 0
+
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created', 'updated', 'is_active']
+    inlines = [QuestionInline]
+
+
+@admin.register(SurveyQuestion)
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    list_display = ['question', 'survey']
+    inlines = [AlternativeInline]
+
+
+@admin.register(SurveyQuestionAlternative)
+class SurveyQuestionAlternativeAdmin(admin.ModelAdmin):
+    list_display = ['alternative', 'question']
