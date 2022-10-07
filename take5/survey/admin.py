91a7773d82ca68365e4f6d52be1e19e6c075ed0a
André de Queiroz <andrepatrinicola@gmail.com>
2022-10-07 15:00:00 -0300

    Cria models, migrations e registra no django admin

--- take5/survey/admin.py
+++ take5/survey/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from survey.models import Survey, SurveyQuestion, SurveyQuestionAlternative, SurveyUserAnswer
+
 # Register your models here.
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+admin.site.register(Survey, SurveyAdmin)
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    list_display = ('survey', 'survey_question')
+
+admin.site.register(SurveyQuestion, SurveyQuestionAdmin)
+class SurveyQuestionAlternativeAdmin(admin.ModelAdmin):
+    list_display = ('survey_question', 'survey_alternative', 'survey_choice_alternative')
+
+admin.site.register(SurveyQuestionAlternative, SurveyQuestionAlternativeAdmin)
+class SurveyUserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'user_answer')
+
+admin.site.register(SurveyUserAnswer, SurveyUserAnswerAdmin)
+
--- take5/survey/models.py
+++ take5/survey/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Survey(models.Model):
+    title = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+
+class SurveyQuestion(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    survey_question = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.survey_question
+
+
+class SurveyQuestionAlternative(models.Model):
+    CHOICES = (
+        ('A','Alternativa A'),
+        ('B','Alternativa B'),
+        ('C','Alternativa C'),
+        ('D','Alternativa D'),
+    )
+
+    survey_question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    survey_alternative = models.CharField(max_length=255)
+    survey_choice_alternative = models.CharField(max_length=1, choices=CHOICES)
+
+    def __str__(self):
+        return self.survey_alternative
+
+class SurveyUserAnswer(models.Model):
+    CHOICES = (
+        ('A', 'Alternativa A'),
+        ('B', 'Alternativa B'),
+        ('C', 'Alternativa C'),
+        ('D', 'Alternativa D')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    user_answer = models.CharField(max_length=1, choices=CHOICES)
+
+
+    def __str__(self):
+        return self.user_answer
