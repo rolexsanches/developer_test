c3c3441117378ff4cadc0184802e61e6d0f8fd6b
André de Queiroz <andrepatrinicola@gmail.com>
2022-10-07 19:26:38 -0300

    adiciona campos extras para serializers

--- take5/survey/serializers.py
+++ take5/survey/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from survey.models import Survey, SurveyQuestion, SurveyQuestionAlternative, SurveyUserAnswer
+from survey.models import Survey, SurveyQuestion, SurveyQuestionAlternative
 
 
 class SurveyQuestionAlternativeSerializer(serializers.ModelSerializer):
@@ -8,21 +8,21 @@ class SurveyQuestionAlternativeSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = SurveyQuestionAlternative
-        fields = '__all__'
+        fields = ['id', 'survey_question', 'survey_alternative', 'survey_choice_alternative']
 
 
 class SurveyQuestionSerializer(serializers.ModelSerializer):
-    
+    survey_alternatives = SurveyQuestionAlternativeSerializer(many=True, read_only=True)
     class Meta:
         model = SurveyQuestion
-        fields = '__all__'
+        fields = ['id', 'survey', 'survey_question', 'survey_alternatives']
 
 
 class SurveySerializer(serializers.ModelSerializer):
-    
+    survey_questions = SurveyQuestionSerializer(many=True, read_only=True) 
     class Meta:
         model = Survey
-        fields = '__all__'
+        fields = ['id','title', 'survey_questions']
 
 
 
--- take5/survey/views.py
+++ take5/survey/views.py
@@ -5,15 +5,18 @@ from survey.serializers import SurveyQuestionAlternativeSerializer, SurveyQuesti
 
 
 class SurveyViewSet(viewsets.ModelViewSet):
+
     queryset = Survey.objects.all() 
     serializer_class = SurveySerializer
 
 class SurveyQuestionViewSet(viewsets.ModelViewSet):
+    
     queryset = SurveyQuestion.objects.prefetch_related('survey').all()
     serializer_class = SurveyQuestionSerializer 
 
 
 class SurveyQuestionAlternativeViewSet(viewsets.ModelViewSet):
+    
     queryset = SurveyQuestionAlternative.objects.prefetch_related('survey_question').all()
     serializer_class = SurveyQuestionAlternativeSerializer
 
