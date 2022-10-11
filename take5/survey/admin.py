456dbf4cd8ea13fa97fab2cf5b06804a4761ffcd
hoxas <hoxas@live.com>
2022-10-11 01:50:39 -0300

    🏄 Registrando models

--- take5/survey/admin.py
+++ take5/survey/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from survey.models import (
+    Survey,
+    SurveyQuestion,
+    SurveyQuestionAlternative,
+    SurveyUserAnswer
+)
 
 # Register your models here.
+
+
+admin.site.register(Survey)
+admin.site.register(SurveyQuestion)
+admin.site.register(SurveyQuestionAlternative)
+admin.site.register(SurveyUserAnswer)
