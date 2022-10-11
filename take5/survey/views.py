6f5ab442a1247188e1b3c882b53daa3966b8e9ec
hoxas <hoxas@live.com>
2022-10-11 17:46:40 -0300

    🤯 Adicionando surveys view

--- take5/survey/views.py
+++ take5/survey/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from survey.models import Survey
+from survey.serializers import SurveySerializer
+from survey.test_utils import calculate_db_queries
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def surveys(request) -> Response:
+    """
+    Rota para recurso Survey
+    """
+
+    if request.method == 'GET':
+        if request.GET.get('id', False):
+            try:
+                query = Survey.objects.prefetch_related('questions').get(
+                    pk=request.query_params['id'])
+                serializer = SurveySerializer(query)
+                calculate_db_queries()
+                return Response(serializer.data, 200)
+            except Survey.DoesNotExist:
+                return Response({'error': 'Survey not found'}, 404)
+        else:
+            query = Survey.objects.prefetch_related(
+                'questions').all()
+            serializer = SurveySerializer(query, many=True)
+            calculate_db_queries()
+            return Response(serializer.data, 200)
