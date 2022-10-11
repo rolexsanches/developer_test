2377aaf9454492650683538e3ae6bc225a933dcc
hoxas <hoxas@live.com>
2022-10-11 03:35:11 -0300

    👽 Registrando filtros no painel adm

--- take5/survey/admin.py
+++ take5/survey/admin.py
@@ -16,12 +16,14 @@ admin.site.register(Survey)
 class SurveyQuestionAdmin(admin.ModelAdmin):
     list_display = ('question', 'survey')
     search_fields = ('question', 'survey__title')
+    list_filter = ('survey__title',)
 
 
 @admin.register(SurveyQuestionAlternative)
 class SurveyQuestionAlternativeAdmin(admin.ModelAdmin):
     list_display = ('alternative', 'question')
     search_fields = ('alternative', 'question__question')
+    list_filter = ('question__question',)
 
 
 @admin.register(SurveyUserAnswer)
@@ -31,3 +33,5 @@ class SurveyUserAnswerAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated',)
     search_fields = ('user__username', 'survey__title',
                      'question__question', 'alternative__alternative')
+    list_filter = ('user__username', 'survey__title',
+                   'question__question', 'alternative__alternative')
